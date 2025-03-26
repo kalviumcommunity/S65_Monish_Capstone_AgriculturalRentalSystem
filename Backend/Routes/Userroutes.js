0c0194b439790ef8c86d3a2a6612f3a91b9a7051
Monish892 <jayachitraramesh08@gmail.com>
2025-03-26 15:34:31 +0530

    Added for Post API

---
+++ Backend/Routes/Userroutes.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const User = require("../Models/User");
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: "User already exists" });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = new User({ name, email, password: hashedPassword, role });
+    await user.save();
+
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+module.exports=router;
