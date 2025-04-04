5228fb36eeb93b4495edfe87afb1bcaf9bbd7d96
Monish892 <jayachitraramesh08@gmail.com>
2025-04-04 10:16:19 +0530

    Implemented JWT authentication

--- Backend/server.js
+++ Backend/server.js
@@ -3,13 +3,21 @@ const app=express()
 const mongoose=require('mongoose')
 const PORT=process.env.PORT || 5000
 require('dotenv').config()
+const cors=require('cors')
 
 
+const userroutes=require('./Routes/Userroutes')
+const equipmentroutes=require('./Routes/Equipmentroutes')
 
+app.use(cors())
 app.use(express.json())
 
 
 mongoose.connect(process.env.MONGO_URI).then(()=>console.log('Connected to Mongodb')).catch(e=>console.log('Not Connected',e))
+
+
+app.use('/api',userroutes)
+app.use('/api',equipmentroutes)
 
 
 
@@ -27,4 +35,6 @@ app.listen(PORT,()=>{
     console.log(`Server is running on the port ${PORT}`);
     
 })
+
+
 
